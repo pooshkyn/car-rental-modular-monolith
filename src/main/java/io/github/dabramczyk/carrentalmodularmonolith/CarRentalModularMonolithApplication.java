e7f2856be1b4cc17a3cbb46beb0363c372e00612
Dariusz Abramczyk <abramczyk.darek@gmail.com>
2020-02-04 20:51:17 +0100

    Init project

---
+++ src/main/java/io/github/dabramczyk/carrentalmodularmonolith/CarRentalModularMonolithApplication.java
@@ -0,0 +1,13 @@
+package io.github.dabramczyk.carrentalmodularmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalModularMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalModularMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dabramczyk/carrentalmodularmonolith/CarRentalModularMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dabramczyk.carrentalmodularmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalModularMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
